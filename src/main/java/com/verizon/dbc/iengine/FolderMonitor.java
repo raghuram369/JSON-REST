dd91b4ed9e5fee59cdf6192a778eb6b9bd326032
VinodSarma <vinodpbhatla@gmail.com>
2016-09-28 19:10:04 +0530

    Update FolderMonitor.java

--- src/main/java/com/verizon/dbc/iengine/FolderMonitor.java
+++ src/main/java/com/verizon/dbc/iengine/FolderMonitor.java
@@ -1,14 +1,24 @@
 package com.verizon.dbc.iengine;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
-public class FolderMonitor{	
-
+//@EnableScheduling
+public class FolderMonitor  implements CommandLineRunner{	
+	
+	@Autowired
+	private FileParser fileParser;
+		
 	public static void main(String[] args) {		
-		SpringApplication.run(FolderMonitor.class, args);
+		SpringApplication.run(FolderMonitor.class, args);	
+		
+	}
+	
+	@Override
+	public void run(String... args) {
+		this.fileParser.fileparser();		
 	}
 	
 	
